75b479efeed517c87b85368f23ba082b6fe9fb89
yinmeili <2697111697@qq.com>
2019-11-12 22:01:29 +0800

    message

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -6,6 +6,7 @@ var filepath=path.join(__dirname,"data.json");
 var content=fs.readFileSync(filepath);
 // console.log(content);
 content=JSON.parse(content);
+var a=content.chapterList;
 // console.log(content);
 
 /* GET home page. */
@@ -18,14 +19,20 @@ router.get('/', function(req, res, next) {
 router.post('/list',function(req,res,next){
     console.log(req);
     var u=req.body;
+    var str;
     if(u.username==content.users[0].username&&u.pwd==content.users[0].password){
-        res.end("success");
+      str="success";
     }else{
-      res.end("error");
+      str="error"
+      
     }
+    res.end(str);
 });
 router.get('/list/add',function(req,res,next){
-  res.render('list');
+  res.render('list',{chapterList:a});
+});
+router.get('/list/add1',function(req,res,next){
+  res.render(err);
 });
 
 
